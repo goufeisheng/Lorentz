e5d20777a4146cef4bd93afe69e2ab0dbbea8c13
goufeisheng <wangyd13@163.com>
2014-10-29 23:50:02 +0800

    Revert "Lorentz fit"
    This reverts commit 2f97e3792f67bc322b9e9aa066978941ca3f7874.

--- lorenz-2/fit_mod.f90
+++
@@ -1,9 +0,0 @@
-module fit_mod
-  real*8,dimension(:),allocatable,save :: datax,datay
-  !real*8,dimension(:,:),allocatable,save :: coeff
-  real*8,dimension(:,:),allocatable,save :: lorentz
-  real*8,dimension(:),allocatable :: centre, width
-  integer,dimension(:),allocatable,save :: lscreen
- integer,save :: nlorentz
-  character*30 :: fitmode
-end module fit_mod
